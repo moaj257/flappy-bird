853bcc8db59e23ad51ec84dc227af87417e8aeb9
moaj257 <moaj257@gmail.com>
2020-05-08 00:05:33 +0530

    Flappy Bird - Skeleton

---
+++ src/components/wall.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View} from 'react-native';
+
+export default class Wall extends React.Component {
+  render() {
+    const {size, body, color} = this.props;
+    const {position} = body;
+    const width = size[0];
+    const height = size[1];
+    const x = position.x - width / 2;
+    const y = position.y - height / 2;
+    
+    return(
+      <View style={{
+        position: 'absolute',
+        top: y,
+        left: x,
+        width,
+        height,
+        backgroundColor: color
+      }} />
+    )
+  }
+}
