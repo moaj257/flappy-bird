fd93843c375e6a41b6c8fd8530dac08ad4d68fd7
moaj257 <moaj257@gmail.com>
2020-05-08 12:51:05 +0530

    added bird some angle

--- src/components/bird.js
+++ src/components/bird.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, Animated} from 'react-native';
 import Images from '../utils/images';
 
 export default class Bird extends React.Component {
+  constructor(props) {
+    super(props);
+    const {body} = this.props;
+    this.animatedValue = new Animated.Value(body.velocity.y);
+  }
+
   render() {
     const {body, pose} = this.props;
     const {position, bounds} = body;
@@ -12,15 +18,23 @@ export default class Bird extends React.Component {
     const x = position.x - width / 2;
     const y = position.y - height / 2;
 
+    this.animatedValue.setValue(body.velocity.y);
+    let rotation = this.animatedValue.interpolate({
+      inputRange: [-10, 0, 10, 20],
+      outputRange: ['-20deg', '0deg', '15deg', '45deg'],
+      extrapolate: 'clamp',
+    })
+
     const image = Images[`bird${pose}`];
     
     return(
-      <Image style={{
+      <Animated.Image style={{
         position: 'absolute',
         top: y,
         left: x,
         width,
         height,
+        transform: [{rotate: rotation}]
       }} resizeMode='stretch' source={image} />
     )
   }
--- src/utils/physics.js
+++ src/utils/physics.js
@@ -6,19 +6,21 @@ let pose = 1;
 
 const Physics = (entities, {touches, time}) => {
   let engine = entities.physics.engine;
+  let world = entities.physics.world;
   let bird = entities.bird.body;
 
+  let hadTouches = false;
   touches.filter(t => t.type === 'press').forEach(t => {
-    Matter.Body.applyForce(bird, bird.position, {x: 0.0, y: -0.1});
+    if(!hadTouches) {
+      if(world.gravity.y === 0.0) {
+        world.gravity.y = 1.2;
+      }
+      hadTouches = true;
+      Matter.Body.setVelocity(bird, {x: bird.velocity.x, y: -10}); 
+    }
   });
 
-  // for(let i = 1; i <= 4; i++) {
-  //   if(entities[`pipe${i}`].body.position.x <= -1 * (Constants.PIPE_WIDTH/2)){
-  //     Matter.Body.setPosition(entities[`pipe${i}`].body, {x: Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH/2), y: entities[`pipe${i}`].body.position.y});
-  //   } else {
-  //     Matter.Body.translate(entities[`pipe${i}`].body, {x: -1, y: 0});
-  //   }
-  // }
+  Matter.Engine.update(engine, time.delta);
 
   Object.keys(entities).forEach(key => {
     if(key.indexOf('floor') === 0) {
@@ -30,9 +32,8 @@ const Physics = (entities, {touches, time}) => {
     }
   });
 
-  Matter.Engine.update(engine, time.delta);
   tick += 1;
-  if(tick % 5 == 0) {
+  if(tick % 9 == 0) {
     pose = pose + 1;
     if(pose > 3) {
       pose = 1;
